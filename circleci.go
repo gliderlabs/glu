55aa1a9257b55b99d87a414791725b7b458dc393
Jeff Lindsay <progrium@gmail.com>
2015-08-20 16:22:58 -0500

    more circleci setup

--- circleci.go
+++ circleci.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/progrium/go-shell"
 	"github.com/spf13/cobra"
@@ -16,6 +17,7 @@ var circleciCmd = &cobra.Command{
 	Use:   "circleci",
 	Short: "Sets up a circleci environment",
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("glu/%s\n\n", Version)
 		defer shell.ErrExit()
 		shell.Trace = true
 		sh("rm -f ~/.gitconfig")
@@ -23,5 +25,10 @@ var circleciCmd = &cobra.Command{
 			build := fmt.Sprintf("build-%s", os.Getenv("CIRCLE_BUILD_NUM"))
 			sh("echo", q(build), "> VERSION")
 		}
+		info := NewProjectInfo()
+		path := shell.Path("/home/ubuntu/.go_workspace/src", info.Repo)
+		sh("rm -rf", path)
+		sh("mkdir -p", filepath.Dir(path))
+		sh("cd .. && mv", info.Name, path, "&&", "ln -s", path, info.Name)
 	},
 }
